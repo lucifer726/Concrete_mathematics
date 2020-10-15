2d8451c74dcf4ca593d7d6c8f5838b5f6c721a70
lucifer726 <18856419225@163.com>
2020-10-15 16:11:19 +0800

    输入a 输出a的素因子分解式

--- test51.py
+++ test51.py
@@ -1,33 +1,38 @@
 # 1.	输入a 输出a的素因子分解式
 import math
 
-primelist = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-
 n = int(input("请输入："))
 
-# newlist中保存素因子
-newlist = []
-# 找到小于根号n的所有素数
-for i in primelist:
-    if i <= math.sqrt(n):
-        newlist.append(i)
+# list中保存素因子
+list = []
+# 找到小于n的所有素数
+for i in range(2, n):
+    for num in range(2, int(math.sqrt(n))):
+        flag = 0
+    if i % num == 0:
+        flag = 1
     else:
-        break
+        list.append(i)
 
 # count中保存阶数
 count = []
+newlist = []
 # 如果a能够整除对应素数，则记录阶数，添加到count中
-for i in newlist:
-    a = n
+for i in list:
     m = 0
-    while a % i == 0:
+    if n % i == 0:
+        newlist.append(i)
+    while n % i == 0:
         m += 1
-        a = a / i
-    count.append(m)
-
+        n = n / i
+    if m > 0:
+        count.append(m)
+sum = sum(count)
+c = 1
 for i in range(len(newlist)):
-    print(newlist[i], end='*')
-    if i != len(newlist) - 1:
-        print(count[i], end='+')
-    else:
-        print(count[i])
+    for j in range(1, count[i] + 1):
+        if c < sum:
+            print(newlist[i], end='*')
+            c += 1
+        else:
+            print(newlist[i])
